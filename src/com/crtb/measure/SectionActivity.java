9486887e9c3ea56b9a5d4e4f60bc1c99194aa3e9
Tsung <username@email.com>
2013-12-06 23:12:30 +0800

    update UI after data sync done.

--- src/com/crtb/measure/SectionActivity.java
+++ src/com/crtb/measure/SectionActivity.java
@@ -16,12 +16,20 @@
 
 package com.crtb.measure;
 
+import java.util.Date;
+
+import com.crtb.measure.data.BasicData;
 import com.crtb.measure.data.BasicInfoDao;
+import com.crtb.measure.service.CrtbWebService;
+import com.crtb.measure.service.IWebService;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -44,6 +52,14 @@ public class SectionActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mPreferences = getSharedPreferences("crtb.xml", MODE_PRIVATE);
+        String userName = mPreferences.getString("username", "");
+        String password = mPreferences.getString("password", "");
+        mBasicData.setUserName(userName);
+        mBasicData.setLoginDate(new Date());
+        mWebService = CrtbWebService.getInstance();
+        mWebService.getZoneAndSiteCodeAsync(userName, password, mHandler);
 
         showSectionView();
     }
@@ -73,10 +89,10 @@ public class SectionActivity extends ListActivity {
                 }
                 int upload = cursor.getInt(COLUMN_UPLOAD);
                 if (columnIndex == COLUMN_SECTION_NAME) {
-                    ((TextView) view).setText(sectionName);
+                    ((TextView)view).setText(sectionName);
                 }
                 if (columnIndex == COLUMN_UPLOAD) {
-                    ((TextView) view).setText(upload > 0 ? R.string.upload : R.string.not_upload);
+                    ((TextView)view).setText(upload > 0 ? R.string.upload : R.string.not_upload);
                 }
                 return true;
             }
@@ -116,4 +132,74 @@ public class SectionActivity extends ListActivity {
     private static final int COLUMN_SECTION_CODE = 4;
 
     private static final int COLUMN_UPLOAD = 6;
+
+    private IWebService mWebService;
+
+    private BasicData mBasicData = new BasicData();
+
+    private int mCount = 0;
+
+    private SharedPreferences mPreferences;
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case IWebService.MSG_GET_ZONE_AND_SITE_CODE_DONE:
+                    String data = (String)msg.obj;
+                    String[] temp = data.split(",");
+                    mBasicData.setZoneCode(temp[0]);
+                    mBasicData.setSiteCode(temp[1]);
+                    if (!TextUtils.isEmpty(mBasicData.getSiteCode())) {
+                        mWebService.getPartInfosAsync(mBasicData.getSiteCode(), mHandler);
+                        mWebService.getSurveyorsAsync(mBasicData.getSiteCode(), mHandler);
+                    }
+                    break;
+                case IWebService.MSG_GET_PART_INFOS_DONE:
+                    for (String name : ((String)msg.obj).split(",")) {
+                        mBasicData.addProject(name);
+                        mWebService.getSectInfosAsync(mBasicData.getSiteCode(), name, mHandler);
+                    }
+                    break;
+                case IWebService.MSG_GET_SECT_INFOS_DONE:
+                    String[] temp2 = ((String)msg.obj).split(":");
+                    String projectName = temp2[0];
+                    for (String sectionCode : temp2[1].split(",")) {
+                        mBasicData.addSectionCode(projectName, sectionCode);
+                        mWebService.getTestCodesAsync(sectionCode, mHandler);
+                        mCount++;
+                    }
+                    break;
+                case IWebService.MSG_GET_TEST_CODES_DONE:
+                    String[] temp3 = ((String)msg.obj).split(":");
+                    String sectionCode = temp3[0];
+                    String innerCodes = temp3[1];
+                    mBasicData.addInnerCodes(sectionCode, innerCodes);
+                    mCount--;
+                    if (mCount == 0) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mBasicData.store();
+                                SectionActivity.this.runOnUiThread(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        refresh();
+                                    }
+                                    
+                                });
+                            }
+                        }).start();
+                    }
+                    break;
+                case IWebService.MSG_GET_SURVEYORS_DONE:
+                    for (String surveyor : ((String)msg.obj).split(",")) {
+                        mBasicData.addSurveyor(surveyor);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        };
+    };
 }
